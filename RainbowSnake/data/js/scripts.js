2ff49177da9e5d4bf255d2315411036d13c3eea4
Gus Class <gguuss@gmail.com>
2016-08-11 23:27:10 -0700

    Adds VU meter boilerplate

--- RainbowSnake/data/js/scripts.js
+++ RainbowSnake/data/js/scripts.js
@@ -315,3 +315,45 @@ var handleCompassEvent = function(e) {
     document.getElementById('headingConsole').innerText = compassHeading + 'deg';
 };
 window.addEventListener('deviceorientation', handleCompassEvent, false);
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+function vuMeter() {
+	if (navigator.getUserMedia) {
+  		navigator.getUserMedia({
+      		audio: true
+    	},
+	    function(stream) {
+	      audioContext = new AudioContext();
+	      analyser = audioContext.createAnalyser();
+	      microphone = audioContext.createMediaStreamSource(stream);
+	      javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+
+	      analyser.smoothingTimeConstant = 0.8;
+	      analyser.fftSize = 1024;
+
+	      microphone.connect(analyser);
+	      analyser.connect(javascriptNode);
+	      javascriptNode.connect(audioContext.destination);
+
+	      javascriptNode.onaudioprocess = function() {
+	          var array = new Uint8Array(analyser.frequencyBinCount);
+	          analyser.getByteFrequencyData(array);
+	          var values = 0;
+
+	          var length = array.length;
+	          for (var i = 0; i < length; i++) {
+	            values += (array[i]);
+	          }
+
+	          var average = values / length;
+	          $('#vumeter').text(Math.floor(average));
+	        } // end fn stream
+	    },
+	    function(err) {
+	      console.log("The following error occured: " + err.name)
+	    });
+	} else {
+	  console.log("getUserMedia not supported");
+	}
+}
+window.setTimeout(function() {vuMeter();}, 100);
